3d63ebf5c97fb73bfd499ecfb388c03e466fb1a1
MrNobodyCode <essadeq.billouche@gmail.com>
2025-02-20 10:48:23 +0100

    add index controller

--- ramad-On/routes/web.php
+++ ramad-On/routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/allPub',function (){
+   return view('allPub');
+});
+
+

57e9d3b764eea8b1d3fbd197dcd4bdda70f0b852
MrNobodyCode <essadeq.billouche@gmail.com>
2025-02-21 17:23:05 +0100

    Add recipe Controllers To Router

--- ramad-On/routes/web.php
+++ ramad-On/routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
+use App\Http\Controllers\RecipesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+// Route for the homepage
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/Posts',[PostController::class, 'index']);
+
+// Post routes
+Route::get('/Posts', [PostController::class, 'index']);
 Route::resource('posts', PostController::class);
 Route::post('/posts/{id}/comment', [PostController::class, 'storeComment'])->name('posts.storeComment');
-
-
-
-
-
+// Recipe Routes
+Route::resource('recipes', RecipesController::class);
+Route::get('recipes', [RecipesController::class, 'index'])->name('recipes.index');
+Route::post('recipes', [RecipesController::class, 'store'])->name('recipes.store');

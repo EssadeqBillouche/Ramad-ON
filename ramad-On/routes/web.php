00f7fd84e701cea0674d9de12a1183e974decaf8
MrNobodyCode <essadeq.billouche@gmail.com>
2025-02-21 11:41:33 +0100

    all Post Done

--- ramad-On/routes/web.php
+++ ramad-On/routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('index');
 });
-
-
 Route::get('/Posts',[PostController::class, 'index']);
 Route::resource('posts', PostController::class);
 Route::post('/posts/{id}/comment', [PostController::class, 'storeComment'])->name('posts.storeComment');

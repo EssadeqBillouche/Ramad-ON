cfd94cd9f711d4bd5af6641f98dfc2c53127dc28
MrNobodyCode <essadeq.billouche@gmail.com>
2025-02-21 17:22:29 +0100

    recipe Model

--- ramad-On/app/Models/Recipe.php
+++ ramad-On/app/Models/Recipe.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Recipe extends Model
@@ -12,20 +12,13 @@ class Recipe extends Model
 
     protected $fillable = ['title', 'instructions', 'category_id'];
 
-    /**
-     * A recipe belongs to a category.
-     */
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    /**
-     * A recipe can have many comments (polymorphic).
-     */
     public function comments(): MorphMany
     {
         return $this->morphMany(Comment::class, 'commentable');
     }
 }
-
